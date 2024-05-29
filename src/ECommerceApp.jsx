6cdf0afebf3ad8ea82cd2d3ed147a9ce77d016bb
Isabel <isabelcristinaorozcop@gmail.com>
2024-05-28 20:29:18 -0500

    Iniciando mi App de E-Commerce

---
+++ src/ECommerceApp.jsx
@@ -0,0 +1,11 @@
+export function ECommerceApp() {
+    return (
+        <div className=" m-auto h-screen">
+            <div className="text-center font-sans font-extrabold">
+                <h1 className="text-3xl m-6">E-Commerce App</h1>
+            </div>
+        </div>
+        
+    )
+    
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {ECommerceApp} from './ECommerceApp'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <ECommerceApp/>
+  </React.StrictMode>,
+)
